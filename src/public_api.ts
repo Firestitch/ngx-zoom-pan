32026213b5d7f352eced9f19df5be045bced6cf2
Raymond Gigliotti <ray@firestitch.com>
2025-02-25 11:59:12 -0500

    fixed mac trackpad zoom

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,3 +1,4 @@
 export { FsZoomPanComponent } from './app/components/zoom-pan/zoom-pan.component';
+export { FsZoomPanContentDirective } from './app/directives/fs-zoom-pan-content.directive';
 export { FsZoomPanModule } from './app/zoom-pan.module';
 
